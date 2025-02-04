2f1bee5ced20134d78ae4bcbc1c3db167e7b28cd
RoyGeova07 <royumana3@gmail.com>
2025-02-04 09:55:15 -0600

    EL BICHO PALMAAA

---
+++ src/pruebaelchel/Prueba_con_Skelerrrrr.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebaelchel;
+
+/**
+ *
+ * @author royum
+ */
+public class Prueba_con_Skelerrrrr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        System.out.println("El bicho palmaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        
+    }
+    
+}
